adb5c7caa19c7dd0d2aa751db0ab1c59ba77b8f4
lflondonoc <141975170+lflondonoc@users.noreply.github.com>
2024-02-20 21:27:15 -0500

    Inicio proyecto universidad

---
+++ src/test/java/co/edu/uniquindio/poo/AppTest.java
@@ -0,0 +1,30 @@
+/**
+ * Clase para probar el funcionamiento del proyecto universidad
+ * @author Área de programación UQ
+ * @since 2024-02
+ * 
+ * Licencia GNU/GPL V3.0 (https://raw.githubusercontent.com/grid-uq/poo/main/LICENSE) 
+ */
+package co.edu.uniquindio.poo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    private static final Logger LOG = Logger.getLogger(AppTest.class.getName());
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void datosCompletos() {
+        LOG.info("Iniciando test datosCompletos");
+       
+
+        LOG.info("Finalizando test datosCompletos");
+    }
+}
